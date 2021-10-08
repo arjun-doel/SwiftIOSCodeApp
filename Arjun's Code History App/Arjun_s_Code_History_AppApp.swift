4bd220568d618e066626497a1eed56fafb8a03fb
Arjun Doel <arjundoel@gmail.com>
2021-10-08 23:44:25 +0100

    Initial Commit

---
+++ Arjun's Code History App/Arjun_s_Code_History_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Arjun_s_Code_History_AppApp.swift
+//  Arjun's Code History App
+//
+//  Created by Arjun Doel on 08/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Arjun_s_Code_History_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
