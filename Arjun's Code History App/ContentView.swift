f1332f08e52696bd657a12125d81fbaaf48e7dce
Arjun Doel <arjundoel@gmail.com>
2021-10-12 20:16:41 +0100

    added three buttons in the HStack

--- Arjun's Code History App/ContentView.swift
+++ Arjun's Code History App/ContentView.swift
@@ -16,10 +16,60 @@ struct ContentView: View {
             mainColor.ignoresSafeArea()
             VStack{
                 Text("1 / 10")
+                    .font(.callout)
+                    .multilineTextAlignment(.leading)
+                    .padding()
                 Text("What was the first computer bug?")
+                    .font(.largeTitle)
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                Spacer()
+                HStack{
+                    Button(action:{
+                        print("Tapped on Choice 1")
+                    },label: {
+                        Text("Ant")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    Button(action:{
+                        print("Tapped on Choice 1")
+                    },label: {
+                        Text("Beetle")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    Button(action:{
+                        print("Tapped on Choice 2")
+                    },label: {
+                        Text("Moth")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    Button(action:{
+                        print("Tapped on Choice 3")
+                    },label: {
+                        Text("Fly")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                }
                 
             }
-        }.foregroundColor(.white)
+        }
+        .foregroundColor(.white)
     }
 }
 
